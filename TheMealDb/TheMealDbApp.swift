0a7645fbb7ef6751a86010e141409e02c8190dea
Mark Schall <schall.mark@gmail.com>
2024-02-14 12:12:36 -0500

    Initial Commit

---
+++ TheMealDb/TheMealDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMealDbApp.swift
+//  TheMealDb
+//
+//  Created by Mark Schall on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMealDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
